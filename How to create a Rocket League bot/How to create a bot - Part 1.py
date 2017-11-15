07fb73abd1adb852ef4f32a12fb4a9b790891beb
Blocks <exploreblocks@gmail.com>
2017-11-15 18:09:33 +0100

    Update for v3

--- How to create a Rocket League bot/How to create a bot - Part 1.py
+++ How to create a Rocket League bot/How to create a bot - Part 1.py
@@ -1,8 +1,6 @@
-BOT_NAME = "TutorialBot"  # You can change this string, if you wish
-
-class agent:
-    def __init__(self, team):
-        self.team = team
+class Agent:
+    def __init__(self, name, team, index):
+        self.index = index
 
     def get_output_vector(self, values):
-        return [16383, 32767, 32767, 0, 0, 0, 0]
+        return [1, 0, 0, 0, 0, False, False, False]
