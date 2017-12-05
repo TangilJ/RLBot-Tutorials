afe4c777af1fc9889c1f2103d37d9afac25d47e6
Blocks <exploreblocks@gmail.com>
2017-12-05 17:54:19 +0100

    Update for v3

--- How to create a Rocket League bot/How to create a bot - Part 4.py
+++ How to create a Rocket League bot/How to create a bot - Part 4.py
@@ -1,39 +1,44 @@
+
 import math
 import time
 
-BOT_NAME = "TutorialBot"  # You can change this string, if you wish
+class Agent:
+    def __init__(self, name, team, index):
+        self.index = index
 
-class agent:
-    def __init__(self, team):
-        self.team = team
+        # Contants
+        self.DODGE_TIME = 0.2
+        self.DISTANCE_TO_DODGE = 500
 
         # Controller inputs
-        self.stick_x = 16383
-        self.stick_y = 16383
-        self.acceleration = 0
-        self.deceleration = 0
-        self.boost = 0
-        self.jump = 0
-        self.powerslide = 0
+        self.throttle = 0
+        self.steer = 0
+        self.pitch = 0
+        self.yaw = 0
+        self.roll = 0
+        self.boost = False
+        self.jump = False
+        self.powerslide = False
 
-        # Game data
-        self.bot_pos_x = 0
-        self.bot_pos_y = 0
-        self.bot_yaw = 0
-        self.ball_pos_x = 0
-        self.ball_pos_y = 0
+        # Game values
+        self.bot_pos = None
+        self.bot_rot = None
+        self.ball_pos = None
+        self.bot_yaw = None
 
         # Dodging
         self.should_dodge = False
         self.on_second_jump = False
         self.next_dodge_time = 0
-        self.DISTANCE_TO_DODGE = 500
+
+    def distance(self, x1, y1, x2, y2):
+        return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
     def aim(self, target_x, target_y):
-        angle_between_bot_and_target = math.degrees(math.atan2(target_x - self.bot_pos_x,
-                                                               target_y - self.bot_pos_y))
+        angle_between_bot_and_target = math.degrees(math.atan2(target_y - self.bot_pos.Y,
+                                                            target_x - self.bot_pos.X))
 
-        angle_front_to_target = angle_between_bot_and_target - self.bot_yaw * -1
+        angle_front_to_target = angle_between_bot_and_target - self.bot_yaw
 
         # Correct the values
         if angle_front_to_target < -180:
@@ -42,79 +47,63 @@ class agent:
             angle_front_to_target -= 360
 
         if angle_front_to_target < -10:
-            # If the target is more than 10 degrees left from the centre, steer right
-            self.stick_x = 32767
-        elif angle_front_to_target > 10:
             # If the target is more than 10 degrees right from the centre, steer left
-            self.stick_x = 0
+            self.steer = -1
+        elif angle_front_to_target > 10:
+            # If the target is more than 10 degrees left from the centre, steer right
+            self.steer = 1
         else:
             # If the target is less than 10 degrees from the centre, steer straight
-            self.stick_x = 16383
+            self.steer = 0
 
-    def dodge(self, target_x, target_y):
+    def check_for_dodge(self, target_x, target_y):
         if self.should_dodge and time.time() > self.next_dodge_time:
             self.aim(target_x, target_y)
-            self.jump = 1
-            self.stick_y = 0
+            self.jump = True
+            self.pitch = -1
 
             if self.on_second_jump:
                 self.on_second_jump = False
                 self.should_dodge = False
             else:
                 self.on_second_jump = True
-                self.next_dodge_time = time.time() + 0.2
-
-    def distance(self, x1, y1, x2, y2):
-        return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+                self.next_dodge_time = time.time() + self.DODGE_TIME
 
     def get_output_vector(self, values):
-        data = values.GameTickPacket
+        # Update game data variables
+        self.bot_pos = values.gamecars[self.index].Location
+        self.bot_rot = values.gamecars[self.index].Rotation
+        self.ball_pos = values.gameball.Location
 
-        # Update ball positions
-        self.ball_pos_x = data.gameball.Location.X
-        self.ball_pos_y = data.gameball.Location.Y  # Y axis is not height here! Z axis is height.
-
-        # Update bot positions and rotations depending on the bot's team
-        if self.team == "blue":
-            team_index = 0
-        else:
-            team_index = 1
-
-        self.bot_pos_x = data.gamecars[team_index].Location.X
-        self.bot_pos_y = data.gamecars[team_index].Location.Y
-
-        # Convert the game's rotation values to degrees
-        self.bot_yaw = abs(data.gamecars[team_index].Rotation.Yaw) % 65536 / 65536 * 360
-        if data.gamecars[team_index].Rotation.Yaw < 0:
+        # Get car's yaw and convert from Unreal Rotator units to degrees
+        self.bot_yaw = abs(self.bot_rot.Yaw) % 65536 / 65536 * 360
+        if self.bot_rot.Yaw < 0:
             self.bot_yaw *= -1
-        # Make 0 degrees the centre of the circle
-        self.bot_yaw = (self.bot_yaw + 90) % 360 - 180
 
         # Blue has their goal at -5000 (Y axis) and orange has their goal at 5000 (Y axis). This means that:
         # - Blue is behind the ball if the ball's Y axis is greater than blue's Y axis
         # - Orange is behind the ball if the ball's Y axis is smaller than orange's Y axis
-        if (self.team == "blue" and self.bot_pos_y < self.ball_pos_y) or (self.team == "orange" and self.bot_pos_y > self.ball_pos_y):
-            self.aim(self.ball_pos_x, self.ball_pos_y)
-            self.acceleration = 32767
+        self.throttle = 1
+        if (self.index == 0 and self.bot_pos.Y < self.ball_pos.Y) or (self.index == 1 and self.bot_pos.Y > self.ball_pos.Y):
+            self.aim(self.ball_pos.X, self.ball_pos.Y)
 
-            if self.distance(self.bot_pos_x, self.bot_pos_y, self.ball_pos_x, self.ball_pos_y) < self.DISTANCE_TO_DODGE:
+            if self.distance(self.bot_pos.X, self.bot_pos.Y, self.ball_pos.X, self.ball_pos.Y) < self.DISTANCE_TO_DODGE:
                 self.should_dodge = True
 
         else:
-            if self.team == "blue":
-                # Blue team's goal is located at (0, 0, -5000)
+            if self.index == 0:
+                # Blue team's goal is located at (0, -5000)
                 self.aim(0, -5000)
-                self.acceleration = 32767
             else:
-                # Orange team's goal is located at (0, 0, 5000)
+                # Orange team's goal is located at (0, 5000)
                 self.aim(0, 5000)
-                self.acceleration = 32767
 
         # This sets self.jump to be active for only 1 frame
-        self.jump = 0
+        self.jump = False
 
-        self.dodge(self.ball_pos_x, self.ball_pos_y)
+        self.check_for_dodge(self.ball_pos.X, self.ball_pos.Y)
 
-        return [self.stick_x, self.stick_y,
-                self.acceleration, self.deceleration,
+        return [self.throttle, self.steer,
+                self.pitch, self.yaw, self.roll,
                 self.jump, self.boost, self.powerslide]
+    
